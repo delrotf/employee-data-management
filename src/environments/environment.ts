e899e31f506dfe74c7556e3e7d9f47df74fa7a12
delrotf <delrotf02@gmail.com>
2017-11-12 04:25:50 +0800

    Add firebaseConfig to environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBkxDAnjTwQ3sIAo8VnMaqU72D3kHt8gcc',
+    authDomain: 'employee-data-management-e025e.firebaseapp.com',
+    databaseURL: 'https://employee-data-management-e025e.firebaseio.com',
+    projectId: 'employee-data-management-e025e',
+    storageBucket: 'employee-data-management-e025e.appspot.com',
+    messagingSenderId: '953026696159'
+  }
 };
