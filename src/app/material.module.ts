a3dc4b224e700dfd700e8ba00d29ef347b6db773
delrotf <delrotf02@gmail.com>
2017-11-13 21:36:28 +0800

    Create country-field component (ng g c country-field --module=app.module)

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { MatInputModule, MatAutocompleteModule, MatRadioModule } from '@angular/material';
+import { MatInputModule, MatAutocompleteModule, MatRadioModule, MatSelectModule } from '@angular/material';
 
 @NgModule ({
-    imports: [MatInputModule, MatAutocompleteModule, MatRadioModule],
-    exports: [MatInputModule, MatAutocompleteModule, MatRadioModule]
+    imports: [MatInputModule, MatAutocompleteModule, MatRadioModule, MatSelectModule],
+    exports: [MatInputModule, MatAutocompleteModule, MatRadioModule, MatSelectModule]
 })
 
 export class MaterialModule {}
