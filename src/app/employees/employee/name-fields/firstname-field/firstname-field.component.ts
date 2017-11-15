25e286aad1d5b633a9a8e249c7c5d455727041c4
delrotf <delrotf02@gmail.com>
2017-11-15 16:45:36 +0800

    Make firstname and lastname fields required.

--- src/app/employees/employee/name-fields/firstname-field/firstname-field.component.ts
+++ src/app/employees/employee/name-fields/firstname-field/firstname-field.component.ts
@@ -1,4 +1,14 @@
+import { ErrorStateMatcher } from '@angular/material/core';
+import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-firstname-field',
@@ -7,6 +17,9 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class FirstnameFieldComponent implements OnInit {
+  myControl = new FormControl('', [Validators.required]);
+
+  matcher = new MyErrorStateMatcher();
 
   constructor() { }
 
--- src/app/employees/employee/name-fields/lastname-field/lastname-field.component.ts
+++ src/app/employees/employee/name-fields/lastname-field/lastname-field.component.ts
@@ -1,4 +1,14 @@
+import { ErrorStateMatcher } from '@angular/material/core';
+import { FormControl, Validators, FormGroupDirective, NgForm } from '@angular/forms';
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-lastname-field',
@@ -7,6 +17,10 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class LastnameFieldComponent implements OnInit {
+  myControl = new FormControl('', [Validators.required]);
+
+  matcher = new MyErrorStateMatcher();
+
 
   constructor() { }
 
