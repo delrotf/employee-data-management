db08807480dbd940b46f153087bf7af737ccf692
delrotf <delrotf02@gmail.com>
2017-11-20 04:19:08 +0800

    Create dateValidator

---
+++ src/app/employees/validators/dateValidator.ts
@@ -0,0 +1,31 @@
+import { AbstractControl, ValidatorFn } from '@angular/forms';
+
+export function minDate(date: Date): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } => {
+    const value = control.value + ' 00:00:00';
+    date.setHours(0);
+    date.setMinutes(0);
+    date.setSeconds(0);
+
+  if (value && new Date(value).valueOf() < date.valueOf()) {
+      console.log('minDate control.value' + new Date(value).valueOf() + ' date:' + date.valueOf());
+      return { 'minDate': { value: value } };
+    }
+    return null;
+  };
+}
+
+export function maxDate(date: Date): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } => {
+    const value = control.value + ' 00:00:00';
+    date.setHours(0);
+    date.setMinutes(0);
+    date.setSeconds(0);
+
+    if (value && new Date(value).valueOf() > date.valueOf()) {
+      console.log('maxDate control.value' + new Date(value).valueOf() + ' date:' + date.valueOf());
+      return { 'maxDate': { value: value } };
+    }
+    return null;
+  };
+}
