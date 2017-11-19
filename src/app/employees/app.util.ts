a3a024eb29bc7c8cc0a827599746c065141ffe6c
delrotf <delrotf02@gmail.com>
2017-11-20 04:19:53 +0800

    Create app.util

---
+++ src/app/employees/app.util.ts
@@ -0,0 +1,11 @@
+export function formatDate(value: Date): string {
+    if (value !== null && value !== undefined) {
+      const year = value.getFullYear();
+      const month = value.getMonth() + 1;
+      const date = value.getDate();
+      // formatted to yyyy-MM-dd
+      const formatted = ((year < 1000) ? '0' + year : year) + '-'
+        + ((month < 10) ? '0' + month : month) + '-' + ((date < 10) ? '0' + date : date);
+      return formatted;
+    }
+  }
