7d013ccf75f83a8e89689205497fe6745ecfec86
delrotf <delrotf02@gmail.com>
2017-11-23 10:07:15 +0800

    Import MatButtonModule.

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatInputModule, MatAutocompleteModule, MatRadioModule, MatSelectModule,
-    MatDatepickerModule, MatNativeDateModule, MatChipsModule, MatIconModule, MatCheckboxModule, MatCardModule } from '@angular/material';
+    MatDatepickerModule, MatNativeDateModule, MatChipsModule, MatIconModule, MatCheckboxModule, 
+    MatCardModule, MatButtonModule } from '@angular/material';
 
 @NgModule ({
     imports: [MatInputModule, MatAutocompleteModule, MatRadioModule, MatSelectModule,
-        MatDatepickerModule, MatNativeDateModule, MatChipsModule, MatIconModule, MatCheckboxModule, MatCardModule],
+        MatDatepickerModule, MatNativeDateModule, MatChipsModule, MatIconModule, MatCheckboxModule, MatCardModule, MatButtonModule],
     exports: [MatInputModule, MatAutocompleteModule, MatRadioModule, MatSelectModule,
-        MatDatepickerModule, MatNativeDateModule, MatChipsModule, MatIconModule, MatCheckboxModule, MatCardModule]
+        MatDatepickerModule, MatNativeDateModule, MatChipsModule, MatIconModule, MatCheckboxModule, MatCardModule, MatButtonModule]
 })
 
 export class MaterialModule {}
