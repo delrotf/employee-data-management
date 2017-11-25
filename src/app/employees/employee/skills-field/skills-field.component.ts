9bffacf8988a0e53d4514194b10cb78a9d1e4453
delrotf <delrotf02@gmail.com>
2017-11-26 03:21:55 +0800

    Fix skills to accept existing data.

--- src/app/employees/employee/skills-field/skills-field.component.ts
+++ src/app/employees/employee/skills-field/skills-field.component.ts
@@ -1,7 +1,8 @@
 import { FormControl } from '@angular/forms';
-import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, Input, OnChanges } from '@angular/core';
 import { ENTER, COMMA } from '@angular/cdk/keycodes';
 import { MatChipInputEvent } from '@angular/material';
+import { Skill } from '../../shared/employee.model';
 
 @Component({
   selector: 'app-skills-field',
@@ -9,23 +10,21 @@ import { MatChipInputEvent } from '@angular/material';
   styleUrls: ['./skills-field.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class SkillsFieldComponent implements OnInit {
+export class SkillsFieldComponent implements OnInit, OnChanges {
   @Input() skillsControl: FormControl;
+  skills: Skill[] = [];
 
   visible = true;
-  selectable = true;
+  selectable = false;
   removable = true;
   addOnBlur = true;
 
   // Enter, comma
   separatorKeysCodes = [ENTER, COMMA];
 
-  skills = [
-  ];
-
-addToControl() {
-  this.skillsControl.setValue(this.skills);
-}
+  addToControl() {
+    this.skillsControl.setValue(this.skills);
+  }
 
   add(event: MatChipInputEvent): void {
     const input = event.input;
@@ -33,7 +32,10 @@ addToControl() {
 
     // Add the item
     if ((value || '').trim()) {
-      this.skills.push({ name: value.trim() });
+      const skill = new Skill();
+      skill.name = value.trim();
+      // this.skills.push({ name: value.trim() });
+      this.skills.push(skill);
     }
 
     // Reset the input value
@@ -54,8 +56,14 @@ addToControl() {
     this.skillsControl = new FormControl('');
   }
 
+  ngOnChanges() {
+    if (this.skillsControl && this.skillsControl.value) {
+      this.skills = this.skillsControl.value;
+    }
+  }
+
   ngOnInit() {
-    this.skillsControl.setValue(this.skills);
+    // this.skillsControl.setValue(this.skills);
   }
 
 }
