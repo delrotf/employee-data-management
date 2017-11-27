5923b8c053b79ae6d9310d125b9aa9800d2953a6
delrotf <delrotf02@gmail.com>
2017-11-28 01:12:01 +0800

    Put firebase database config to environment prod.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  edmFirebaseConfig: {
+    apiKey: 'AIzaSyBkxDAnjTwQ3sIAo8VnMaqU72D3kHt8gcc',
+    authDomain: 'employee-data-management-e025e.firebaseapp.com',
+    databaseURL: 'https://employee-data-management-e025e.firebaseio.com',
+    projectId: 'employee-data-management-e025e',
+    storageBucket: 'employee-data-management-e025e.appspot.com',
+    messagingSenderId: '953026696159'
+  }
 };
